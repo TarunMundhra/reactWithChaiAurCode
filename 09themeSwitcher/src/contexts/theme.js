a302a005d5031aa049e56dd3e9626069842f76af
TarunMundhra <lthamundhra5@gmail.com>
2026-01-04 10:11:40 +0530

    feat: initialize theme switcher project with React and Tailwind CSS
    - Added package.json with necessary dependencies and scripts for development.
    - Included Vite configuration for React and Tailwind CSS.
    - Created main application structure with App component, theme context, and theme switching functionality.
    - Implemented Card component to display product information.
    - Added styles for the application using Tailwind CSS.
    - Included SVG assets for branding.
    - Set up initial CSS for dark mode support.

---
+++ 09themeSwitcher/src/contexts/theme.js
@@ -0,0 +1,14 @@
+import { createContext  , useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode : "Light",
+    darkTheme : () => {},
+    lightTheme : () => {},
+})
+
+export const  ThemeProvider = ThemeContext.Provider
+
+// custom hooks
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
